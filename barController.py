e88149d6a33bf81d5865034b28805c3d050d1f05
Rajat Mehndiratta <rajatmehndiratta@cmu.edu>
2015-02-28 12:57:10 -0500

    whoops. xrange -> range

--- barController.py
+++ barController.py
@@ -36,7 +36,7 @@ import time
 def setup(servos):
     pwm = [] * len(servos)
     GPIO.setmode(GPIO.BCM)
-    for x in xrange(len(servos)):
+    for x in range(len(servos)):
         GPIO.setup(servos[x], GPIO.OUT)
         pwm[x] = GPIO.PWM(servos[x], 100)
         pwm[x].start(5)
@@ -48,20 +48,25 @@ def update(ingredient, angle):
 def pour(instructions):
     assert(type(instructions) == list)
     assert(len(instructions) == 5)
-    for x in xrange(1, 5):
+    for x in range(1, 5):
         assert(type(instructions[x]) == float)
         assert(instructions[x] >= 0)
         assert(instructions[x] <= 1)
     volume = instructions[0]
     servos = [18, 19, 20, 21]
     setup(servos)
-    for x in xrange(1, 5):
+    for x in range(1, 5):
         update(x, 90)
     flowRate = 2 # seconds per milliliter
     amounts = [(float(volume) * instructions[x]) for
-               x in xrange(1, len(instructions))]
+               x in range(1, len(instructions))]
     flowTimes = [flowRate * amount for amount in amounts]
     for i in len(flowTimes):
         update(i+1, 180)
         time.sleep(flowTimes[i])
         update(i+1, 90)
+
+
+
+
+
