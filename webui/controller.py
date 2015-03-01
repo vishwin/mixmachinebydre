52d4f7672e02bba480455823bae4b9b3224171fe
Charlie Li <git@vishwin.info>
2015-02-28 22:29:15 -0500

    Define some views, albeit commented out, for the GPIO calls

--- webui/controller.py
+++ webui/controller.py
@@ -1,4 +1,5 @@
 from webui import app
+from flask import request, render_template
 try:
 	import RPi.GPIO as GPIO
 except:
@@ -7,3 +8,15 @@ except:
 @app.route("/")
 def sup():
 	return "Dr Dre's Mix Machine. No, it has nothing to do with music."
+
+#@app.route("/ingredients", methods=['POST'])
+
+#@app.route("/mix", methods=['POST'])
+
+#@app.route("/lucky")
+
+#@app.route("/rate", methods=['GET'])
+
+#@app.route("/firehose")
+#def configureGPIO():
+	
